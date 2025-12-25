fa4fe30a61f26b2abb09c68e58d3ff0bdddda2a5
bparth2312 <157197215+bparth2312@users.noreply.github.com>
2025-12-25 12:23:25 +0530

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -1,1 +1,19 @@
-process.env.RAZORPAY_KEY_SECRET
+import Razorpay from "razorpay";
+
+export default async function handler(req, res) {
+  try {
+    const razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+
+    const order = await razorpay.orders.create({
+      amount: 50000, // ₹500 (amount in paise)
+      currency: "INR",
+    });
+
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
